df4378b12800f65ebfd15f0250c28b875c775e03
Dmitry Surin <dmitry.surin@gmail.com>
2021-07-28 17:31:00 +0300

    Add zsh config

--- install.sh
+++ install.sh
@@ -11,3 +11,9 @@ ln -s "$HOME/dotfiles/X11" "$HOME/.config"
 # i3
 rm -rf "$HOME/.config/i3"
 ln -s "$HOME/dotfiles/i3" "$HOME/.config"
+
+# zsh
+mkdir -p "$HOME/.config/zsh"
+ln -sf "$HOME/dotfiles/zsh/.zshenv" "$HOME"
+ln -sf "$HOME/dotfiles/zsh/.zshrc" "$HOME/.config/zsh"
+
