e2b840623db55900e72cee7c7ea309a74ef4aa65
Dmitry Surin <dmitry.surin@gmail.com>
2023-09-06 21:20:02 +0400

    New provision code

--- pwsh/Scripts/wsl/reset_and_provision_ubuntu_wsl.ps1
+++ pwsh/Scripts/wsl/reset_and_provision_ubuntu_wsl.ps1
@@ -1,8 +1,16 @@
 $ErrorActionPreference = "Stop"
 
+$sw = [Diagnostics.Stopwatch]::StartNew()
+
 $user = "bedware"
 ./reset_ubuntu_wsl.ps1 $user $env:PASS
-Write-Host "Copying WSL provisioning files"
-Copy-Item -Recurse "$env:DOTFILES/ansible/wsl" "\\wsl.localhost\Ubuntu-22.04\home\$user"
-Write-Host "Copying SSH-keys"
-Copy-Item -Recurse "$env:HOME/.ssh" "\\wsl.localhost\Ubuntu-22.04\home\$user"
+Write-Host "Preparing for provioning"
+$ubuntu = wsl -l | ForEach-Object { $_ -replace "`0", "" } |  Select-String -Pattern "ubuntu" | Select-Object -First 1
+wsl -d $ubuntu --cd ~ -- git clone https://github.com/bedware/.dotfiles.git
+Write-Host "dotfiles successfylly cloned to WSL"
+# Copy-Item -Recurse "$env:DOTFILES/ansible/wsl" "\\wsl.localhost\Ubuntu-22.04\home\$user"
+# Write-Host "Copying SSH-keys"
+# Copy-Item -Recurse "$env:HOME/.ssh" "\\wsl.localhost\Ubuntu-22.04\home\$user"
+
+$sw.Stop()
+"$($sw.Elapsed.Seconds) seconds, $($sw.Elapsed.Milliseconds) millis"
--- pwsh/Scripts/wsl/reset_ubuntu_wsl.ps1
+++ pwsh/Scripts/wsl/reset_ubuntu_wsl.ps1
@@ -8,18 +8,19 @@ $ErrorActionPreference = "Stop"
 $sw = [Diagnostics.Stopwatch]::StartNew()
 
 $ubuntu = wsl -l | ForEach-Object { $_ -replace "`0", "" } |  Select-String -Pattern "ubuntu" | Select-Object -First 1
+wsl --terminate $ubuntu
+wsl --unregister $ubuntu
 ubuntu2204 install --root
 if ([string]::IsNullOrEmpty($ubuntu)) {
     $ubuntu = wsl -l | ForEach-Object { $_ -replace "`0", "" } |  Select-String -Pattern "ubuntu" | Select-Object -First 1
-} else {
     wsl --terminate $ubuntu
     wsl --unregister $ubuntu
+    wsl -d $ubuntu /bin/bash -c "useradd -m -s /bin/bash -G adm,sudo,dip,plugdev,netdev $user"
+    wsl -d $ubuntu /bin/bash -c "echo $user`:$pass | chpasswd"
+    ubuntu2204 config --default-user $user
 }
 $ubuntu = $ubuntu.ToString()
 Write-Host "Profile name: $ubuntu"
 
-wsl -d $ubuntu /bin/bash -c "useradd -m -s /bin/bash -G adm,sudo,dip,plugdev,netdev $user"
-wsl -d $ubuntu /bin/bash -c "echo $user`:$pass | chpasswd"
-ubuntu2204 config --default-user $user
 $sw.Stop()
 "$($sw.Elapsed.Seconds) seconds, $($sw.Elapsed.Milliseconds) millis"
