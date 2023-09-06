ec634825d955044da4437892e5555882492abb19
Dmitry Surin <dmitry.surin@gmail.com>
2023-09-06 17:35:04 +0400

    Added pwsh script, zig for treesitter

--- pwsh/Scripts/wsl/reset_and_provision_ubuntu_wsl.ps1
+++ pwsh/Scripts/wsl/reset_and_provision_ubuntu_wsl.ps1
@@ -2,4 +2,7 @@ $ErrorActionPreference = "Stop"
 
 $user = "bedware"
 ./reset_ubuntu_wsl.ps1 $user $env:PASS
-Copy-Item -Recurse "$env:DOTFILES/ansible/wsl" "\\wsl.localhost\Ubuntu-22.04\home\$user\wsl_init"
+Write-Host "Copying WSL provisioning files"
+Copy-Item -Recurse "$env:DOTFILES/ansible/wsl" "\\wsl.localhost\Ubuntu-22.04\home\$user"
+Write-Host "Copying SSH-keys"
+Copy-Item -Recurse "$env:HOME/.ssh" "\\wsl.localhost\Ubuntu-22.04\home\$user"
