cc3f988b96c80072151448a76e6780c40d396124
Dmitry Surin <dmitry.surin@gmail.com>
2023-09-15 00:46:59 +0400

    Added pass ask for WSL reset

--- pwsh/wsl_reset_and_provision_ubuntu.ps1
+++ pwsh/wsl_reset_and_provision_ubuntu.ps1
@@ -3,14 +3,12 @@ $ErrorActionPreference = "Stop"
 $sw = [Diagnostics.Stopwatch]::StartNew()
 
 $user = "bedware"
-./reset_ubuntu_wsl.ps1 $user $env:PASS
+$pass = Read-Host -Prompt "Type your password" -MaskInput
+./wsl_reset_ubuntu.ps1 $user $pass
 Write-Host "Preparing for proviosioning"
 $ubuntu = wsl -l | ForEach-Object { $_ -replace "`0", "" } |  Select-String -Pattern "ubuntu" | Select-Object -First 1
 wsl -d $ubuntu --cd ~ -- git clone https://github.com/bedware/.dotfiles.git
 Write-Host "dotfiles successfully cloned to WSL"
-# Copy-Item -Recurse "$env:DOTFILES/ansible/wsl" "\\wsl.localhost\Ubuntu-22.04\home\$user"
-# Write-Host "Copying SSH-keys"
-# Copy-Item -Recurse "$env:HOME/.ssh" "\\wsl.localhost\Ubuntu-22.04\home\$user"
 
 $sw.Stop()
 "$($sw.Elapsed.Seconds) seconds, $($sw.Elapsed.Milliseconds) millis"
