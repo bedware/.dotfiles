46c8b3b43592b0615ffad03699cb94a4e7900382
Dmitry Surin <dmitry.surin@gmail.com>
2023-09-15 21:55:29 +0400

    Added git configs & zsh scripts

---
+++ wsl/zsh/.local/zsh/custom/example.zsh
@@ -0,0 +1,24 @@
+# You can put files here to add functionality separated per file, which
+# will be ignored by git.
+# Files on the custom/ directory will be automatically loaded by the init
+# script, in alphabetical order.
+
+# For example: add yourself some shortcuts to projects you often work on.
+#
+# brainstormr=~/Projects/development/planetargon/brainstormr
+# cd $brainstormr
+
+# function hello() { echo "Hello world"; }
+# zle -N hello
+# bindkey '^g' hello
+
+# function featbranch() {
+#   branch_name=$BUFFER
+#   zle backward-kill-line
+#   echo "git checkout -b feat/$branch_name"
+#   git checkout -b "feat/${branch_name}"
+#   zle reset-prompt
+#   zle redisplay
+# }
+# zle -N featbranch
+# bindkey '^gf' featbranch
