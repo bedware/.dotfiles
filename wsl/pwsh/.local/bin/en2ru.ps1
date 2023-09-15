34ddfbd0b222b8fb7e2088023077e28a5bf2c9ad
Dmitry Surin <dmitry.surin@gmail.com>
2023-09-15 21:56:45 +0400

    new permitions

--- wsl/pwsh/.local/bin/en2ru.ps1
+++ wsl/pwsh/.local/bin/en2ru.ps1
@@ -1,4 +1,4 @@
 curl --location --request POST 'https://translate.yandex.net/api/v1/tr.json/translate?id=2f77af58.63c41428.4e0dba2e.74722d74657874-6-0&srv=tr-text&source_lang=en&target_lang=ru' `
->> --header 'Content-Type: application/x-www-form-urlencoded' `
->> --data-urlencode 'text=What are you doing?'
+--header 'Content-Type: application/x-www-form-urlencoded' `
+--data-urlencode 'text=What are you doing?'
 
