ef109f75b1e7cb49a8dc3cc13d66ba56034404e1
Dmitry Surin <dmitry.surin@gmail.com>
2023-09-22 20:34:30 +0400

    ChatGPT plugin added & copy-mode binding

--- wsl/nvim/.config/nvim/lua/bedware/packer.lua
+++ wsl/nvim/.config/nvim/lua/bedware/packer.lua
@@ -47,4 +47,15 @@ return require('packer').startup(function(use)
             { 'rafamadriz/friendly-snippets' },
         }
     }
+    use({
+        "jackMort/ChatGPT.nvim",
+        config = function()
+            require("chatgpt").setup()
+        end,
+        requires = {
+            "MunifTanjim/nui.nvim",
+            "nvim-lua/plenary.nvim",
+            "nvim-telescope/telescope.nvim"
+        }
+    })
 end)
