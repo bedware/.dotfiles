fa0197165bd3628aba6583470986398929d3f54b
Dmitry Surin <dmitry.surin@gmail.com>
2024-08-29 00:28:24 +0400

    Updated fuzzy script

--- wsl/pwsh/.local/bin/update-fuzzy-env-vars.ps1
+++ wsl/pwsh/.local/bin/update-fuzzy-env-vars.ps1
@@ -9,19 +9,29 @@ $FD_GLOBAL_FIND_DIRECTORY_COMMAND = "fd --type d --no-ignore $FdDefaultOptions"
 $FILES_IN_GIT_COMMAND = "git ls-files"
 
 function Put-VarToEnv($envVarName, $envVarValue) {
-    $regPathSystem = "HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager\Environment"
-    $regPathUser = "HKCU:\Environment"
+    if ($IsWindows) {
+        $regPathUser = "HKCU:\Environment"
 
-    # Убедиться, что $envVarName является строкой
-    $envVarName = [string]$envVarName
-    $envVarValue = [string]$envVarValue
+        $envVarName = [string]$envVarName
+        $envVarValue = [string]$envVarValue
 
-    # Добавление переменной в пользовательские переменные
-    if (-not (Test-Path -Path "$regPathUser\$envVarName")) {
-        Set-ItemProperty -Path $regPathUser -Name $envVarName -Value $envVarValue
-        Write-Output "Пользовательская переменная $envVarName была добавлена с значением $envVarValue."
+        if (-not (Test-Path -Path "$regPathUser\$envVarName")) {
+            Set-ItemProperty -Path $regPathUser -Name $envVarName -Value $envVarValue
+            Write-Output "Пользовательская переменная $envVarName была добавлена с значением $envVarValue."
+        } else {
+            Write-Output "Пользовательская переменная $envVarName уже существует в $regPathUser."
+        }
+    } elseif ($IsLinux) {
+        $bashProfilePath = "$HOME/.bashrc"
+
+        if (-not (Select-String -Path $bashProfilePath -Pattern "export $envVarName=")) {
+            Add-Content -Path $bashProfilePath -Value "export $envVarName='$envVarValue'"
+            Write-Output "Переменная $envVarName была добавлена в $bashProfilePath с значением $envVarValue."
+        } else {
+            Write-Output "Переменная $envVarName уже существует в $bashProfilePath."
+        }
     } else {
-        Write-Output "Пользовательская переменная $envVarName уже существует."
+        Write-Output "Неизвестная платформа. Скрипт поддерживает только Windows и Linux."
     }
 }
 
