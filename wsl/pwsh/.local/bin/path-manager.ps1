b6295d0452b0e0ed05b3db8e7e544a2288b0054c
Dmitry Surin <dmitry.surin@gmail.com>
2024-09-23 00:05:54 +0400

    Changed mechanism of path manager

--- wsl/pwsh/.local/bin/path-manager.ps1
+++ wsl/pwsh/.local/bin/path-manager.ps1
@@ -9,12 +9,6 @@ param(
     [string]$AppVersion
 )
 
-if ($IsWindows) {
-    $SYSTEM_APP_DIR = "C:\Windows"
-} elseif ($IsLinux) {
-    $SYSTEM_APP_DIR = "/bin"
-}
-
 $candidates = @{}
 # Java base
 $candidates["java"] = "$env:HOME/.sdkman/candidates/java"
@@ -28,59 +22,124 @@ $candidates["karaf"] = "$env:HOME/.sdkman/candidates/karaf"
 $candidates["springboot"] = "$env:HOME/.sdkman/candidates/springboot"
 $candidates["labctl"] = "$env:HOME/.local/.iximiuz"
 
-if ($candidates.ContainsKey($App)) {
-    if ($AppVersion) {
-        Write-Host "Path argument is not empty"
-        if (Test-Path ($candidates[$App] + "/$AppVersion")) {
-            $selected = $AppVersion
-        } else {
-            Write-Warning "Candidate passed as an argument ($AppVersion) doesnt exist!"
-            return
-        }
-    } else {
-        $selected = Get-ChildItem $candidates[$App] | Select-Object -ExpandProperty Name | Invoke-Fzf
+class PathManager {
+    [hashtable]$candidates
+
+    PathManager($candidates){
+        $this.candidates = $candidates
     }
 
-    $target = $candidates[$App]
+    # factory method
+    [void] setCandidate($App, $Mode, $AppVersion) {
+        $this._checkApp($App)
+        $selected = $this._findSelectedVersion($App, $AppVersion)
+        $target = $this.candidates[$App]
 
-    if ($IsWindows) {
-        $App = "$App.exe"
-    }
+        $bin = "$target/$selected/bin"
 
-    switch ($Mode) {
-        "default" {
-            $link = "$SYSTEM_APP_DIR/$App"
-            Write-Host $link
-
-            $target = "$target/$selected/bin/$App"
-            Write-Host $target
-
-            if ($IsWindows) {
-                New-Item -Force -ItemType SymbolicLink -Path $link -Target $target
-            } elseif ($IsLinux) {
-                Invoke-Expression "sudo ln -f -s $target $link"
+        Write-Host "$Mode mode"
+        switch ($Mode) {
+            "default" {
+                Write-Host "target: $target, selected: $selected"
+                $this.setDefaultCandidate($target, $selected)
+            }
+            "shell" {
+                Write-Host "bin: $bin"
+                $this.setShellCandidate($bin)
             }
         }
-        "shell" {
-            $target = "$target/$selected/bin"
-            Write-Host $target
+    }
 
-            $tmp = ,$target + ($env:PATH -split [IO.Path]::PathSeparator)
-            $env:PATH = ($tmp -join [IO.Path]::PathSeparator)
+    [void] setDefaultCandidate($target, $selected) {
+        throw("Must be overriden")
+    }
+
+    [void] setShellCandidate($bin) {
+        $tmp = ,$bin + ($env:PATH -split [IO.Path]::PathSeparator)
+        $env:PATH = ($tmp -join [IO.Path]::PathSeparator)
+    }
+
+    hidden [void] _checkApp($App) {
+        if (!$this.candidates.ContainsKey($App)) {
+            throw("Sorry that candidate ($App) doesn't supported. Check '$PSCommandPath'.")
         }
     }
-} else {
-    Write-Host "Sorry that candidate ($App) doesn't supported. Check '$PSCommandPath'."
-}
+    hidden [string] _findSelectedVersion($App, $AppVersion){
+        $pathToCandidate = $this.candidates[$App]
+        if (-not (Test-Path $pathToCandidate)) {
+            throw("Candidate passed as an argument ($pathToCandidate) doesn't exist!")
+        }
+        if ($AppVersion) {
+            $pathToCandidate = $this.candidates[$App] + "/$AppVersion"
+            if (Test-Path ($pathToCandidate)) {
+                $selected = $AppVersion
+            } else {
+                throw("Candidate passed as an argument ($pathToCandidate) doesn't exist!")
+            }
+        } else {
+            $selected = Get-ChildItem $this.candidates[$App] | Select-Object -ExpandProperty Name | Invoke-Fzf
+        }
+        return $selected
+    }
 
-function Test-CommandInPath($command){
-    if (Get-Command $command -ErrorAction SilentlyContinue) {
-        Write-Output "Command '$command' is available."
-        return $true
-    } else {
-        Write-Output "Command '$command' is not available."
-        return $false
+    # testCommandInPath("java")
+    static [bool] testCommandInPath($command) {
+        if (Get-Command $command -ErrorAction SilentlyContinue) {
+            Write-Output "Command '$command' is available."
+            return $true
+        } else {
+            Write-Output "Command '$command' is not available."
+            return $false
+        }
+    }
+
+    static [PathManager] getInstance($candidates) {
+        if ($global:IsWindows) {
+            return [WindowsPathManager]::new($candidates)
+        } else {
+            return [LinuxPathManager]::new($candidates)
+        }
     }
 }
-# Test-CommandInPath("java")
-#
+
+class WindowsPathManager : PathManager {
+
+    WindowsPathManager([hashtable]$candidates) : base($candidates) {}
+
+    [void] setDefaultCandidate($target, $selected) {
+        $defaultLink = "$target/default"
+        New-Item -Force -ItemType SymbolicLink -Path $defaultLink -Target "$target/$selected"
+
+        $defaultBinLink = "$defaultLink/bin"
+        $pathContent = [Environment]::GetEnvironmentVariable("PATH", [EnvironmentVariableTarget]::User) -split [IO.Path]::PathSeparator
+        if (!$pathContent.Contains($defaultBinLink)) {
+            $pathContent += $defaultBinLink
+            [Environment]::SetEnvironmentVariable("PATH", ($pathContent -join [IO.Path]::PathSeparator), [EnvironmentVariableTarget]::User)
+            Write-Host "Restart of session is required"
+        }
+    }
+}
+
+class LinuxPathManager : PathManager {
+
+    LinuxPathManager([hashtable]$candidates) : base($candidates) {}
+
+    [void] setDefaultCandidate($target, $selected) {
+        $defaultLink = "$target/default"
+        New-Item -Force -ItemType SymbolicLink -Path $defaultLink -Target "$target/$selected"
+
+        $defaultBinLink = "$defaultLink/bin"
+        $pathContent = [Environment]::GetEnvironmentVariable("PATH", [EnvironmentVariableTarget]::Process) -split [IO.Path]::PathSeparator
+        if (!$pathContent.Contains($defaultBinLink)) {
+            $check = 'export PATH="$PATH:' + $defaultBinLink + '"' 
+            $candidateExistInProfile = Select-String -Path "$env:HOME/.profile" -Pattern "^$check$"
+            if (!$candidateExistInProfile) {
+                Add-Content -Path "$env:HOME/.profile" -Value $check
+                Write-Host "Restart of session is required"
+            }
+        }
+    }
+}
+
+[PathManager]::getInstance($candidates).setCandidate($App, $Mode, $AppVersion)
+
